4fd4c5542ad1769efefd597e8093524985602317
atai2 <1114677814@qq.com>
2018-01-31 22:42:33 +0800

    init activiti

--- atActiviti/src/main/java/com/atsho/activitimanage/ActivitiApplication.java
+++ atActiviti/src/main/java/com/atsho/activitimanage/ActivitiApplication.java
@@ -3,8 +3,10 @@ package com.atsho.activitimanage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 //@EnableEurekaClient
 @EnableAutoConfiguration(exclude = {
         org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration.class,
