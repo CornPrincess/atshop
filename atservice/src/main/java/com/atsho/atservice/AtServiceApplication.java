45811ff411e4168c74796bfa42e70f74c32e7894
atai2 <1114677814@qq.com>
2018-06-09 02:28:38 +0800

    zk

---
+++ atservice/src/main/java/com/atsho/atservice/AtServiceApplication.java
@@ -0,0 +1,12 @@
+package com.atsho.atservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtServiceApplication.class, args);
+	}
+}
