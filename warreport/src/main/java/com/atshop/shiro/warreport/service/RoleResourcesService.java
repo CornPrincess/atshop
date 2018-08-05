3760a77acc3624a6499442eb7acf2d270aa66ab5
ATai2 <1114677814@qq.com>
2018-08-06 00:26:54 +0800

    shiro

---
+++ warreport/src/main/java/com/atshop/shiro/warreport/service/RoleResourcesService.java
@@ -0,0 +1,11 @@
+package com.atshop.shiro.warreport.service;
+
+
+import com.atshop.shiro.warreport.model.RoleResources;
+
+/**
+ * Created by yangqj on 2017/4/26.
+ */
+public interface RoleResourcesService extends IService<RoleResources>  {
+    public void addRoleResources(RoleResources roleResources);
+}
---
+++ warreport/src/main/java/com/atshop/shiro/warreport/service/UserRoleService.java
@@ -0,0 +1,12 @@
+package com.atshop.shiro.warreport.service;
+
+
+import com.atshop.shiro.warreport.model.UserRole;
+
+/**
+ * Created by yangqj on 2017/4/26.
+ */
+public interface UserRoleService extends IService<UserRole> {
+
+    public void addUserRole(UserRole userRole);
+}
