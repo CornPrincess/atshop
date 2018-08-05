7df01be351973603144942b0ea1d4a17f63df4a2
atai2 <1114677814@qq.com>
2018-08-05 13:24:27 +0800

    hadoop client

---
+++ shifeiwarreport/src/main/java/com/atshop/shifeiwarreport/ShifeiwarreportApplication.java
@@ -0,0 +1,12 @@
+package com.atshop.shifeiwarreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShifeiwarreportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShifeiwarreportApplication.class, args);
+    }
+}
