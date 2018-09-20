de135d1022dd81e8a8953157c2c1063facd210b7
ATai2 <1114677814@qq.com>
2018-09-20 21:26:35 +0800

    shiro

--- warreport/src/main/java/com/atshop/shiro/warreport/mapper/RoleResourcesMapper.java
+++
@@ -1,8 +0,0 @@
-package com.atshop.shiro.warreport.mapper;
-
-
-import com.atshop.shiro.warreport.model.RoleResources;
-import com.atshop.shiro.warreport.util.MyMapper;
-
-public interface RoleResourcesMapper extends MyMapper<RoleResources> {
-}
