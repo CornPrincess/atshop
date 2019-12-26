0d6cae15824c4edd5f4d5e238cea02fa9c4549a4
ATai2 <1114677814@qq.com>
2019-12-27 00:58:45 +0800

    init

--- swaggerdoc/src/main/java/com/ppx/swaggerdoc/People.java
+++ swaggerdoc/src/main/java/com/ppx/swaggerdoc/People.java
@@ -1,8 +1,14 @@
 package com.ppx.swaggerdoc;
 
+import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
+@ApiModel
 public class People {
-    @ApiModelProperty(value = "defaultOutputStr",example="mockOutputStrValue")
+    @ApiModelProperty(value = "defaultOutputStr", example = "mockOutputStrValue")
     private String name;
+    @ApiModelProperty(value = "defaulnameOutputStr", example = "mockOutputStrValue")
+    private String name2;
+    @ApiModelProperty(value = "defaulnamenametOutputStr", example = "mockOutputStrValue")
+    private String name3;
 }
--- swaggerdoc/src/main/java/com/ppx/swaggerdoc/RestController.java
+++ swaggerdoc/src/main/java/com/ppx/swaggerdoc/RestController.java
@@ -5,13 +5,15 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @org.springframework.web.bind.annotation.RestController
 @Api(value = "用户信息管理")
 public class RestController {
     @ApiOperation(value = "获取用户信息", notes = "通过用户ID获取用户信息")
-    @GetMapping("/test")
-    public People getUser(@ApiParam(value = "用户ID",required = true) int id) {
+    @PostMapping("/test")
+    public People getUser(@ApiParam(value = "用户ID",required = true) @RequestBody People people) {
         return new People();
     }
 }
