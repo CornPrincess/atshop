e57f83f2ca058ca67b458f3ee3f507789d3c9a7d
ATai2 <1114677814@qq.com>
2019-12-29 13:50:56 +0800

    init

---
+++ swaggerdoc/src/test/java/com/ppx/swaggerdoc/SwaggerdocApplicationTest.java
@@ -0,0 +1,13 @@
+package com.ppx.swaggerdoc;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SwaggerdocApplicationTest {
+
+    @Test
+    public void test(){
+        System.out.println(System.currentTimeMillis());
+    }
+}
