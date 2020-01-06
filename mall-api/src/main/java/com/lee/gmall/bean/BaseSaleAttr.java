9adf9fb54bfbd821f3bca81fd393a76d399742d3
ATai2 <1114677814@qq.com>
2020-01-06 20:03:42 +0800

    package confilict

--- mall-api/src/main/java/com/lee/gmall/bean/BaseSaleAttr.java
+++ mall-api/src/main/java/com/lee/gmall/bean/BaseSaleAttr.java
@@ -1,28 +1,13 @@
 package com.lee.gmall.bean;
 
-import java.io.Serializable;
+import lombok.Data;
 
+import java.io.Serializable;
+@Data
 public class BaseSaleAttr implements Serializable {
 
   private String id;
   private String name;
 
-
-  public String getId() {
-    return id;
-  }
-
-  public void setId(String id) {
-    this.id = id;
-  }
-
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
 
 }
--- mall-api/src/main/java/com/lee/gmall/bean/CartInfo.java
+++ mall-api/src/main/java/com/lee/gmall/bean/CartInfo.java
@@ -1,8 +1,10 @@
 package com.lee.gmall.bean;
 
+import lombok.Data;
+
 import java.io.Serializable;
 import java.math.BigDecimal;
-
+@Data
 public class CartInfo implements Serializable {
 
   private String id;
@@ -15,95 +17,4 @@ public class CartInfo implements Serializable {
   private BigDecimal skuPrice;
   private Integer skuNum;
   private String skuName;
-
-
-  public String getId() {
-    return id;
-  }
-
-  public void setId(String id) {
-    this.id = id;
-  }
-
-
-  public String getUserId() {
-    return userId;
-  }
-
-  public void setUserId(String userId) {
-    this.userId = userId;
-  }
-
-
-  public String getSkuId() {
-    return skuId;
-  }
-
-  public void setSkuId(String skuId) {
-    this.skuId = skuId;
-  }
-
-
-  public BigDecimal getCartPrice() {
-    return cartPrice;
-  }
-
-  public void setCartPrice(BigDecimal cartPrice) {
-    this.cartPrice = cartPrice;
-  }
-
-
-  public long getQuantity() {
-    return quantity;
-  }
-
-  public void setQuantity(long quantity) {
-    this.quantity = quantity;
-  }
-
-
-  public String getImgUrl() {
-    return imgUrl;
-  }
-
-  public void setImgUrl(String imgUrl) {
-    this.imgUrl = imgUrl;
-  }
-
-
-  public String getIsChecked() {
-    return isChecked;
-  }
-
-  public void setIsChecked(String isChecked) {
-    this.isChecked = isChecked;
-  }
-
-
-  public BigDecimal getSkuPrice() {
-    return skuPrice;
-  }
-
-  public void setSkuPrice(BigDecimal skuPrice) {
-    this.skuPrice = skuPrice;
-  }
-
-
-  public Integer getSkuNum() {
-    return skuNum;
-  }
-
-  public void setSkuNum(Integer skuNum) {
-    this.skuNum = skuNum;
-  }
-
-
-  public String getSkuName() {
-    return skuName;
-  }
-
-  public void setSkuName(String skuName) {
-    this.skuName = skuName;
-  }
-
 }
